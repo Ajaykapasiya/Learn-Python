d0261b45da44568251bcba0342391bf661d8f3d8
ajay <ajaykumar290904@gmail.com>
2026-02-18 12:28:38 +0530

    Add initial Python scripts for basic exercises and examples

---
+++ Python/Hello.py
@@ -0,0 +1,171 @@
+"""print("Enter two number")
+a = int(input())
+b = int(input())
+
+c = a+b
+print("the sum is",c)"""
+
+
+"""print("Enter the number")
+marks = int(input())
+
+if(marks>90):
+    print("Grade A")
+elif(marks>80):
+    print("Grade B")
+elif(marks>70):
+    print("Grade C")
+elif(marks>60):
+    print("Grade D")
+elif(marks>50):
+    print("Grade E")
+else:
+    print("Grade F")"""
+
+"""print("Enter the number")
+x = int(input())
+
+print("even") if (x%2 == 0) else print("Odd")"""
+
+
+"""x = 1
+while x <= 5:
+  print("Hello")
+  x = x+1"""
+
+
+"""x = int(input("Enter the number: "))
+i = 1
+y = 0
+while i<=x:
+        y = y + i
+        i+=1
+print("the sum is:", y)"""
+
+
+"""n = int(input("Enter the number: "))
+i = 2
+while i<=n-1:
+    if n%i==0:
+        break
+    i+=1
+
+if i==n:
+    print("Prime")
+else:
+    print("Non Prime")"""
+
+
+
+# print("Enter the Number")
+# a = int(input())
+
+# for a in range(1,a+1):
+#     print(a,'=',a*a)
+
+
+# l1 = [1,2,3,4,5]
+# # for x in l1:
+# #     print(x)
+# l1.append(6)
+# l1.insert(2,2.5)
+# print(l1)
+
+
+# i = 1
+# while i<=10:
+#     print(2*i-1)
+#     i+=1
+
+# for i in range(1,11):
+#     print(2*i-1,end=' ')
+# print()
+
+# n = int(input())
+# sum = 0
+
+# for a in range(1,n+1):
+#     sum = sum + a
+
+# print(sum)
+
+
+# l1 = [1,2,3,4,5,6]
+# i = 0
+# while i<=5:
+#     print(l1[i],end=' ')
+#     i+=1
+
+# for x in l1:
+#     print(x)
+
+
+# l1 = [1,2,3,4,5,6]
+# print(len(l1))
+# print(max(l1))
+# print(min(l1))
+# print(sum(l1))
+# print(sorted(l1))
+
+# l2 = [3,4,6,3,5,2]
+# del l2[3]
+
+
+# print(l2)
+
+# s1 = 'Hey my name is ajay'
+# # print(type(s1))
+# print(len(s1))
+# print(s1.index('y'))
+# print(s1.count('a '))
+# print(s1.startswith('Hey'))
+# print(s1.isupper())
+# print(s1.replace('a','A'))
+# print(s1)
+
+
+# s1 = 'Hello how are you'
+# s2 = s1.split(' ')
+# s3 = "-".join(s2)
+# print(s3)
+
+# s1 = ['Java', 'Test']
+
+# s1.reverse()
+# print(s1)
+
+
+# s1 = ['Java', 'Test']
+
+# result = [x[::-1] for x in s1[::-1]]
+# print(result)
+
+# result.reverse()
+# print(result)
+
+# s1 = input("Enter the string: ")
+# rev = s1.split(' ')[::-1]
+# print(" ".join(rev))
+
+# t = (1,2,3,4,5,6,7)
+# for x in t:
+#     print(x)
+
+# t1 = ([int(e) for e in input().split(',')])
+# print(t1)
+
+
+# s = "Python Is Fun"
+# print(" ".join(word[::-1] for word in s.split(' ')[::-1]))
+
+l1 =[10,20,40,10,50,20,50,60]
+
+l2 = list(set(l1))
+print(l2)
+
+
+
+
+
+
+
