81ffbbe5660b2fab37c1dd7fd5a4f5754d52b048
ajay <ajaykumar290904@gmail.com>
2026-02-20 23:40:00 +0530

    Add new patterns and number printing exercises to program.py and String.py

--- Python/String.py
+++ Python/String.py
@@ -97,3 +97,6 @@
 
 
 
+n = 5 
+for i in range(n,0-1):
+    print("x"*i)
--- Python/program.py
+++ Python/program.py
@@ -250,6 +250,58 @@
 # print("Type:", type(s))
 
 
+# n = 5 
+# for i in range(1,n+1):
+#     print(" " * (n-i) + "*" * (2*i-1) )
+
+# for i in range(n,0,-1):
+#     print(" " * (n-i) + "*" * (2*i-1))
+
+
+# n = 5 
+# for i in range(1,n+1):
+#     for j in range(1,i+1):
+#         print(j,end=" ")
+#         print()
 
 
 
+# n = 5
+# for i in range(1,n+1):
+#     print(str(i)*i)
+
+# n=5
+# for i in range(n):
+#     print("*"*n)
+
+
+# n = 5 
+# for i in range(1,n+1):
+#     for j in range(1,i+1):
+#         print(j,end=" ")
+#     print()
+
+
+# n = 5
+# num = 1
+
+# for i in range(1,n+1):
+#     for j in range(i):
+#         print(num,end=" ")
+#         num += 1
+#     print()
+
+# n= 5
+# for i in range(1,n+1):
+#     print(" " * (n-i),end=" ")
+#     for j in range(1,i+1):
+#         print(j,end=" ")
+#     for j in range(i-1,0,-1):
+#         print(j,end=" ")
+#     print()    
+
+# n= 5
+# for i in range(1,n):
+#     print("*" * i)
+# for i in range(n,0,-1):
+#     print("*" * i)
